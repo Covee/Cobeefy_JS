a8a13302fa361dae24e60bd7536024933ac7ece3
Cobee <uniqtop91@gmail.com>
2019-03-10 22:10:36 +0900

    made Shopiing List Controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/ListView';
 import { elements, renderLoader, clearLoader } from './views/base';
 import Recipe from './models/Recipe';
+import List from './models/List';
 
 
 const state = {};
+window.state = state;
 
 const controlSearch = async () => {
     const query = searchView.getInput();
@@ -82,6 +85,29 @@ const controlRecipe = async() => {
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+// List Controller
+const controlList = () => {
+    if (!state.list) state.list = new List();
+    
+    state.recipe.ingredient.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    })
+}
+
+// Handle delete and update list item
+elements.shopping.addEventListener('click', e => { 
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    if (e.target.matches('.shopping__delete', '.shopping__delete *')) {
+        state.list.deleteItem(id);
+        listView.deleteItem(id);
+    } else if (e.target.matches('.shopping__count-value')) {
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+})
+
 elements.recipe.addEventListener('click', e => {
     if (e.target.matches('.btn-decrease, .btn-decrease *')) {
         if (state.recipe.servings > 1) {
@@ -91,6 +117,10 @@ elements.recipe.addEventListener('click', e => {
     } else if (e.target.matches('.btn-increase, .btn-increase *')) {
         state.recipe.updateServings('inc')
         recipeView.updateServingsIngredients(state.recipe)
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        controlList();
     }
-    console.log(state.recipe)
 })
+
+
+window.l = new List();
